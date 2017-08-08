1f27f2b1e4b764e566c388defaa5a8f14a728fbc
Igor Rodionov <goruha@gmail.com>
2017-08-09 01:57:37 +0600

    Grant perms

--- main.tf
+++ main.tf
@@ -17,26 +17,42 @@ resource "aws_s3_bucket" "default" {
   }
 }
 
-data "aws_iam_policy_document" "codepipeline" {
+resource "aws_iam_role" "default" {
+  name = "${module.label.id}"
+
+  assume_role_policy = "${data.aws_iam_policy_document.assume.json}"
+}
+
+
+data "aws_iam_policy_document" "assume" {
   statement {
     sid = ""
 
     actions = [
-      "s3:GetObject",
-      "s3:GetObjectVersion",
-      "s3:GetBucketVersioning",
-      "s3:PutObject",
+      "sts:AssumeRole",
     ]
 
-    resources = [
-      "${aws_s3_bucket.default.arn}",
-      "${aws_s3_bucket.default.arn}/*",
-      "arn:aws:s3:::elasticbeanstalk*",
-    ]
+    principals {
+      type        = "Service"
+      identifiers = ["codepipeline.amazonaws.com"]
+    }
 
     effect = "Allow"
   }
+}
 
+resource "aws_iam_role_policy_attachment" "default" {
+  role   = "${aws_iam_role.default.id}"
+  policy_arn = "${aws_iam_policy.default.arn}"
+}
+
+
+resource "aws_iam_policy" "default" {
+  name   = "${module.label.id}"
+  policy = "${data.aws_iam_policy_document.default.json}"
+}
+
+data "aws_iam_policy_document" "default" {
   statement {
     sid = ""
 
@@ -58,7 +74,51 @@ data "aws_iam_policy_document" "codepipeline" {
     resources = ["*"]
     effect    = "Allow"
   }
+}
 
+resource "aws_iam_role_policy_attachment" "s3" {
+  role   = "${aws_iam_role.default.id}"
+  policy_arn = "${aws_iam_policy.s3.arn}"
+}
+
+resource "aws_iam_policy" "s3" {
+  name   = "${module.label.id}-s3"
+  policy = "${data.aws_iam_policy_document.s3.json}"
+}
+
+data "aws_iam_policy_document" "s3" {
+  statement {
+    sid = ""
+
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectVersion",
+      "s3:GetBucketVersioning",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "${aws_s3_bucket.default.arn}",
+      "${aws_s3_bucket.default.arn}/*",
+      "arn:aws:s3:::elasticbeanstalk*",
+    ]
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild" {
+  role   = "${aws_iam_role.default.id}"
+  policy_arn = "${aws_iam_policy.codebuild.arn}"
+}
+
+
+resource "aws_iam_policy" "codebuild" {
+  name   = "${module.label.id}-codebuild"
+  policy = "${data.aws_iam_policy_document.codebuild.json}"
+}
+
+data "aws_iam_policy_document" "codebuild" {
   statement {
     sid = ""
 
@@ -69,37 +129,9 @@ data "aws_iam_policy_document" "codepipeline" {
     resources = ["${module.build.project_id}"]
     effect    = "Allow"
   }
-
 }
 
-data "aws_iam_policy_document" "assume" {
-  statement {
-    sid = ""
 
-    actions = [
-      "sts:AssumeRole",
-    ]
-
-    principals {
-      type        = "Service"
-      identifiers = ["codepipeline.amazonaws.com"]
-    }
-
-    effect = "Allow"
-  }
-}
-
-resource "aws_iam_role" "default" {
-  name = "${module.label.id}"
-
-  assume_role_policy = "${data.aws_iam_policy_document.assume.json}"
-}
-
-resource "aws_iam_role_policy" "codepipeline" {
-  name   = "${module.label.id}"
-  role   = "${aws_iam_role.default.id}"
-  policy = "${data.aws_iam_policy_document.codepipeline.json}"
-}
 
 module "build" {
   source    = "git::https://github.com/cloudposse/tf_codebuild.git?ref=init"
@@ -109,6 +141,11 @@ module "build" {
 
   image         = "${var.build_image}"
   instance_size = "${var.build_instance_size}"
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_s3" {
+  role   = "${module.build.role_arn}"
+  policy_arn = "${aws_iam_policy.s3.arn}"
 }
 
 resource "aws_codepipeline" "default" {
