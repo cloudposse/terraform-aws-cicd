97ef119ae6d6d14cbddfe64e64a32c327fff5424
aknysh <aknysh>
2017-09-01 23:15:04 -0400

    Add variables for `tf_codebuild`

--- variables.tf
+++ variables.tf
@@ -27,7 +27,8 @@ variable "repo_name" {}
 variable "branch" {}
 
 variable "build_image" {
-  default = "alpine"
+  default     = "aws/codebuild/docker:1.12.1"
+  description = "Docker image for build environment, e.g. 'aws/codebuild/docker:1.12.1' or 'aws/codebuild/eb-nodejs-6.10.0-amazonlinux-64:4.0.0'. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref.html"
 }
 
 variable "build_instance_size" {
@@ -47,4 +48,33 @@ variable "attributes" {
 variable "tags" {
   type    = "map"
   default = {}
+}
+
+variable "privileged_mode" {
+  default     = false
+  description = "(Optional) If set to true, enables running the Docker daemon inside a Docker container on the CodeBuild instance. Used when building Docker images"
+}
+
+variable "aws_region" {
+  type        = "string"
+  default     = ""
+  description = "(Optional) AWS Region, e.g. us-east-1. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "aws_account_id" {
+  type        = "string"
+  default     = ""
+  description = "(Optional) AWS Account ID. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "image_repo_name" {
+  type        = "string"
+  default     = ""
+  description = "(Optional) ECR repository name to store the Docker image built by this module. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "image_tag" {
+  type        = "string"
+  default     = ""
+  description = "(Optional) Docker image tag in the ECR repository, e.g. 'latest'. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
 }
