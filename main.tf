76c8ab14593344c29aa12f976679d8156497df50
Igor Rodionov <goruha@gmail.com>
2017-11-21 08:58:23 +0600

    Bump version

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ data "aws_iam_policy_document" "codebuild" {
 }
 
 module "build" {
-  source             = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=feature-generalize-env-vars"
+  source             = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=tags/0.6.0"
   namespace          = "${var.namespace}"
   name               = "${var.name}"
   stage              = "${var.stage}"
