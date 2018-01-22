8a82fef6eb2587c553a45b16150d4dc7070eff70
Igor Rodionov <goruha@gmail.com>
2018-01-22 10:28:46 +0600

    Update README (#16)
    * Update README
    
    * Fix linting

--- variables.tf
+++ variables.tf
@@ -1,49 +1,64 @@
 variable "namespace" {
-  default = "global"
+  default     = "global"
+  description = "Namespace, which could be your organization name, e.g. 'cp' or 'cloudposse'"
 }
 
 variable "stage" {
-  default = "default"
+  default     = "default"
+  description = "Stage, e.g. 'prod', 'staging', 'dev', or 'test'"
 }
 
 variable "name" {
-  default = "app"
+  default     = "app"
+  description = "Solution name, e.g. 'app' or 'jenkins'"
 }
 
 variable "enabled" {
-  default = "true"
+  default     = "true"
+  description = "Enable ``CodePipeline`` creation"
 }
 
 variable "app" {
-  type    = "string"
-  default = ""
+  type        = "string"
+  default     = ""
+  description = "Elastic Beanstalk application name. If not provided or set to empty string, the ``Deploy`` stage of the pipeline will not be created"
 }
 
 variable "env" {
-  type    = "string"
-  default = ""
+  type        = "string"
+  default     = ""
+  description = "Elastic Beanstalk environment name. If not provided or set to empty string, the ``Deploy`` stage of the pipeline will not be created"
 }
 
-variable "github_oauth_token" {}
+variable "github_oauth_token" {
+  description = "GitHub Oauth Token with permissions to access private repositories"
+}
 
-variable "repo_owner" {}
+variable "repo_owner" {
+  description = "GitHub Organization or Person name"
+}
 
-variable "repo_name" {}
+variable "repo_name" {
+  description = "GitHub repository name of the application to be built (and deployed to Elastic Beanstalk if configured)"
+}
 
-variable "branch" {}
+variable "branch" {
+  description = "Branch of the GitHub repository, _e.g._ ``master``"
+}
 
 variable "build_image" {
   default     = "aws/codebuild/docker:1.12.1"
-  description = "Docker image for build environment, e.g. 'aws/codebuild/docker:1.12.1' or 'aws/codebuild/eb-nodejs-6.10.0-amazonlinux-64:4.0.0'. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref.html"
+  description = "Docker image for build environment, _e.g._ `aws/codebuild/docker:1.12.1` or `aws/codebuild/eb-nodejs-6.10.0-amazonlinux-64:4.0.0`"
 }
 
 variable "build_compute_type" {
-  default = "BUILD_GENERAL1_SMALL"
+  default     = "BUILD_GENERAL1_SMALL"
+  description = "`CodeBuild` instance size.  Possible values are: ```BUILD_GENERAL1_SMALL``` ```BUILD_GENERAL1_MEDIUM``` ```BUILD_GENERAL1_LARGE```"
 }
 
 variable "buildspec" {
   default     = ""
-  description = "Optional buildspec declaration to use for building the project"
+  description = " Declaration to use for building the project. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/build-spec-ref.html)"
 }
 
 # https://www.terraform.io/docs/configuration/variables.html
@@ -55,45 +70,48 @@ variable "poll_source_changes" {
 }
 
 variable "delimiter" {
-  type    = "string"
-  default = "-"
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
 }
 
 variable "attributes" {
-  type    = "list"
-  default = []
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
 }
 
 variable "tags" {
-  type    = "map"
-  default = {}
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. `map('BusinessUnit', 'XYZ')`"
 }
 
 variable "privileged_mode" {
   default     = "false"
-  description = "(Optional) If set to true, enables running the Docker daemon inside a Docker container on the CodeBuild instance. Used when building Docker images"
+  description = "If set to true, enables running the Docker daemon inside a Docker container on the CodeBuild instance. Used when building Docker images"
 }
 
 variable "aws_region" {
   type        = "string"
   default     = ""
-  description = "(Optional) AWS Region, e.g. us-east-1. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+  description = "AWS Region, e.g. us-east-1. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
 }
 
 variable "aws_account_id" {
   type        = "string"
   default     = ""
-  description = "(Optional) AWS Account ID. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+  description = "AWS Account ID. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
 }
 
 variable "image_repo_name" {
   type        = "string"
   default     = "UNSET"
-  description = "(Optional) ECR repository name to store the Docker image built by this module. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+  description = "ECR repository name to store the Docker image built by this module. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
 }
 
 variable "image_tag" {
   type        = "string"
   default     = "latest"
-  description = "(Optional) Docker image tag in the ECR repository, e.g. 'latest'. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+  description = "Docker image tag in the ECR repository, e.g. 'latest'. Used as CodeBuild ENV variable when building Docker images. [For more info](http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html)"
 }
