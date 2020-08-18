329462e9fd0dd9c78fdd2c2043588aef93598411
Matt Gowie <gowie.matt@gmail.com>
2020-08-18 01:12:32 -0600

    [AUTOMATED] Update terraform-null-label versions to support Terraform 0.13 (#68)

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "aws_region" "default" {
 }
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.15.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
   enabled    = var.enabled
   namespace  = var.namespace
   name       = var.name
