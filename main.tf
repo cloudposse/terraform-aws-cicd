bf48fbe63e18e1b652f2ee7501a6a80bd854cd1a
Sree Teja <potlurusreeteja1997@gmail.com>
2022-03-25 09:42:06 +0530

    fix: clean s3 resource and codebuild module points to cloudposse

--- main.tf
+++ main.tf
@@ -16,36 +16,10 @@ resource "aws_s3_bucket" "default" {
   #bridgecrew:skip=BC_AWS_S3_13:Skipping `Enable S3 Bucket Logging` check until bridgecrew will support dynamic blocks (https://github.com/bridgecrewio/checkov/issues/776).
   #bridgecrew:skip=BC_AWS_S3_14:Skipping `Ensure all data stored in the S3 bucket is securely encrypted at rest` check until bridgecrew will support dynamic blocks (https://github.com/bridgecrewio/checkov/issues/776).
   #bridgecrew:skip=CKV_AWS_52:Skipping `Ensure S3 bucket has MFA delete enabled` due to issue in terraform (https://github.com/hashicorp/terraform-provider-aws/issues/629).
-  count  = local.enabled ? 1 : 0
-  bucket = module.this.id
-  #acl           = "private"
+  count         = local.enabled ? 1 : 0
+  bucket        = module.this.id
   force_destroy = var.force_destroy
   tags          = module.this.tags
-
-  # versioning {
-  #   enabled = var.versioning_enabled
-  # }
-
-  # dynamic "logging" {
-  #   for_each = var.access_log_bucket_name != "" ? [1] : []
-  #   content {
-  #     target_bucket = var.access_log_bucket_name
-  #     target_prefix = "logs/${module.this.id}/"
-  #   }
-  # }
-
-  # dynamic "server_side_encryption_configuration" {
-  #   for_each = var.s3_bucket_encryption_enabled ? [1] : []
-
-  #   content {
-  #     rule {
-  #       apply_server_side_encryption_by_default {
-  #         sse_algorithm = "AES256"
-  #       }
-  #     }
-  #   }
-  # }
-
 }
 
 # S3 acl resource support for AWS provider V4
@@ -238,7 +212,8 @@ data "aws_iam_policy_document" "codebuild" {
 }
 
 module "codebuild" {
-  source                      = "git::https://github.com/axerinc/terraform-aws-codebuild.git?ref=refactor-s3-resource-v4"
+  source                      = "cloudposse/codebuild/aws"
+  version                     = "0.38.0"
   build_image                 = var.build_image
   build_compute_type          = var.build_compute_type
   buildspec                   = var.buildspec
