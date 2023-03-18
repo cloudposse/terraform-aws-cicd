874f6a0cc94f7089a6c405408ba564d331ff6155
Trey McMeans <tluck234@gmail.com>
2023-03-18 12:12:13 -0500

    Block public access

--- main.tf
+++ main.tf
@@ -48,6 +48,15 @@ resource "aws_s3_bucket" "default" {
 
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.default.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_iam_role" "default" {
   count              = local.enabled ? 1 : 0
   name               = module.this.id
